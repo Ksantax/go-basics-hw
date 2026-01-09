2348fd1f99d2a413148beca695e33d7cf4aabcb4
Alexander Chekhov <chekhovsasha4737@gmail.com>
2026-01-09 23:56:17 +0300

    first fix

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -5,9 +5,10 @@ import "fmt"
 const (
 	USDtoEUR = 0.93
 	USDtoRUB = 72.10
+	EURtoRUB = USDtoRUB / USDtoEUR
 )
 
 func main() {
-	result := (50 / USDtoEUR) * USDtoRUB
+	result := 50 * EURtoRUB
 	fmt.Printf("50 EUR равняется %.2f RUB\n", result)
 }
